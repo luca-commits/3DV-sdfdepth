58d732fbce3e443e181843e39ce1083d94597c59
LRabuzin <lovro.rabuzin@gmail.com>
2023-03-30 23:58:53 +0200

    finish up training code

--- models.py
+++ models.py
@@ -1,8 +1,10 @@
 import segmentation_models_pytorch as smp
 
-unet = smp.Unet(
-    encoder_name="resnet34",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
-    encoder_weights="imagenet",     # use `imagenet` pre-trained weights for encoder initialization
-    in_channels=1,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
-    classes=3,                      # model output channels (number of classes in your dataset)
-)
+def get_unet(encoder_type: str = "resnet34"):
+    unet = smp.Unet(
+        encoder_name="resnet34",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+        encoder_weights="imagenet",     # use `imagenet` pre-trained weights for encoder initialization
+        in_channels=3,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
+        classes=1,                      # model output channels (number of classes in your dataset)
+    )
+    return unet
--- train.py
+++ train.py
@@ -1,55 +1,54 @@
 import pandas as pd
 import numpy as np
 import os
-from dataset import HistoneMarksDataset
 import torch
 from torch.utils.data import DataLoader
-from models import unet
-from utils import *
+from models import get_unet
+from train_utils import train
 import argparse
+import wandb
+from datasets import MonoDepthDataset
 
-def main(modeltag: str):
+def main(config):    
+    val_dataset = MonoDepthDataset(img_dir=os.join(config["data_dir"],config["rgb_image_dir"]),
+                                   target_dir=os.join(config["data_dir"], config["depth_val_image_dir"]))
+    train_dataset = MonoDepthDataset(img_dir=os.join(config["data_dir"], config["rgb_image_dir"]),
+                                     target_dir=os.join(config["data_dir"], config["depth_train_image_dir"]))
 
-    # load data
-    data_path = f"{os.getcwd()}/data"
-    version = "unnormalised_200000_bin_200"
-    
-    
-    val_dataset = MonoDepthDataset(img_dir=os.join(data_path, "rgb_images"), target_dir=os.join(data_path, "data_depth_annotated"))
-    train_dataset = MonoDepthDataset()
+    val_data_loader = DataLoader(val_dataset, batch_size=config["batch_size"], shuffle=False)
+    train_data_loader = DataLoader(train_dataset, batch_size=config["batch_size"], shuffle=True)
 
+    model = get_unet(config["encoder_type"])
 
+    results = train(model, train_data_loader, val_data_loader, config)
 
-    train_data_loader = DataLoader(train_data, batch_size=512, shuffle=True)
-    val_data_loader = DataLoader(val_data, batch_size=512, shuffle=True)
-
-    model = unet
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    train_args = {
-        "epochs": 50, 
-        "device": device,
-        "scheduler": "ReduceLROnPlateau",
-        "optimizer_args": { "lr": 0.0005},
-        "verbose": True,
-    }
-
-    print(train_args)
-    results = train(model, train_data_loader, val_data_loader, train_args)
-
-    pretrained_path = f"{os.getcwd()}/pretrained"
+    pretrained_path = os.path.join(config["data_dir"], "pretrained")
     if not os.path.exists(pretrained_path):
-        # if the demo_folder directory is not present 
-        # then create it.
         os.makedirs(pretrained_path)
 
-    model_path = f"{pretrained_path}/{modeltag}"
+    model_path = os.path.join(pretrained_path, f"{config['experiment_description']}_{config['encoder_type']}.pth")
     torch.save(results["model"].state_dict(), model_path)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Train CNN')
-    parser.add_argument('--modeltag', type=str, required=True)
+    parser = argparse.ArgumentParser(description='Train a monocular depth estimation network')
+
+    parser.add_argument('--experiment_description', default='')
+    parser.add_argument('--encoder_type', default='resnet34')
+    parser.add_argument('--batch_size', default=32, type=int)
+    parser.add_argument('--max_epochs', default=50, type=int)
+    parser.add_argument('--lr', default=0.0001, type=float)
+    parser.add_argument('--momentum', type=float, default=0.9)
+    parser.add_argument('--data_dir', default='/cluster/scratch/lrabuzin/data')
+    parser.add_argument('--rgb_image_dir', default='KITTI/raw_data')
+    parser.add_argument('--depth_train_image_dir', default='KITTI/train')
+    parser.add_argument('--depth_val_image_dir', default='KITTI/val')
+
     args = parser.parse_args()
-    main(modeltag=args.modeltag)
+
+    wandb.init(project="3DV_sdfdepth", config=vars(args))
+    wandb.config['device'] = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    config = wandb.config
+
+    main(config)
