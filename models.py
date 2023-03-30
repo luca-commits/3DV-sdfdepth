da6e35840deaabeaca77c6d88139e20f56a7613d
Luca Wolfart <wolfartl@ethz.ch>
2023-03-30 16:17:59 +0200

    commit

---
+++ models.py
@@ -0,0 +1,8 @@
+import segmentation_models_pytorch as smp
+
+unet = smp.Unet(
+    encoder_name="resnet34",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+    encoder_weights="imagenet",     # use `imagenet` pre-trained weights for encoder initialization
+    in_channels=1,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
+    classes=3,                      # model output channels (number of classes in your dataset)
+)
