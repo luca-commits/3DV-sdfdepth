d018bc4338aaedc67ea29d9cc3dae787fa8e8398
LRabuzin <lovro.rabuzin@gmail.com>
2023-05-14 12:53:11 +0200

    make render matrix homogenous

--- render.py
+++ render.py
@@ -199,6 +199,8 @@ class RenderTrajectory:
         cy = torch.stack(cy)
         camera_to_worlds = torch.stack(camera_to_worlds)
         
+        camera_to_worlds[:, 0:3, 1:3] *= -1
+
         num_images = len(camera_to_worlds)
 
         camera_to_worlds_1 = camera_to_worlds[:(num_images//2), :, :]
@@ -243,7 +245,6 @@ class RenderTrajectory:
          # TODO: figure out if this is needed
         
         # Convert from COLMAP's/OPENCV's camera coordinate system to nerfstudio
-        camera_to_worlds[:, 0:3, 1:3] *= -1
 
         camera_to_worlds = torch.tensor(camera_to_worlds).float()
 
