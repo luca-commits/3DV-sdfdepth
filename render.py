0 degree rotation bugfix

render.py
@@ -218,8 +218,8 @@ class RenderTrajectory:
 
         rotmats_i1 = []
         rotmats_i2 = []
-        rotmat_1 = get_rotmat(0)
-        rotmat_2 = get_rotmat(0)
+        rotmat_out_1 = get_rotmat(0)
+        rotmat_out_2 = get_rotmat(0)
         for i in range (len(rotmats_1)):
             rotmat_1 = rotmats_1[i]
             rotmat_2 = rotmats_2[i]
@@ -230,8 +230,8 @@ class RenderTrajectory:
             rotmat_i = slerp(0.5)
             rotmat_i = rotmat_i.as_matrix()
 
-            rotmat_i1 = np.dot(rotmat_1, rotmat_i)
-            rotmat_i2 = np.dot(rotmat_2, rotmat_i)
+            rotmat_i1 = np.dot(rotmat_out_1, rotmat_i)
+            rotmat_i2 = np.dot(rotmat_out_2, rotmat_i)
 
             rotmats_i1.append(rotmat_i1)
             rotmats_i2.append(rotmat_i2)