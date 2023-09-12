30f1572cbe583b50ff581c3f94a4d1fb2c06be68
Niall Siegenheim <sniall@student.ethz.ch>
2023-09-12 15:58:10 +0200

    array wise submission

--- training/nerfstudio/submit_training.sh
+++ training/nerfstudio/submit_training.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 #SBATCH -c 8
-#SBATCH --time=12:00:00
+#SBATCH --time=4:00:00
 #SBATCH --mem-per-cpu=4G
 #SBATCH --gpus=rtx_3090:1
 #SBATCH -A s_stud_infk 
 #SBATCH --job-name=nerfstudio_training
 #SBATCH --output=nerfstudio_training.out
 #SBATCH --error=nerfstudio_training.err
+#SBATCH --array=0-4
 
+datasets=(/cluster/project/infk/courses/252-0579-00L/group26/sniall/kitti/datasets_cvpr/*/)
+datasets=$(echo $datasets | xargs -n1 | sort | xargs)
 
-SCENE_NAME=2011_09_26_drive_0001_sync_0
+data_path=${datasets[$SLURM_ARRAY_TASK_ID]}
 
 module purge
 module load gcc/8.2.0 cuda/11.8.0 python/3.9.9 eth_proxy
@@ -18,7 +21,7 @@ source /cluster/project/infk/courses/252-0579-00L/group26/nerfstudio/venv/bin/ac
 
 ns-train depth-nerfacto-huge \
 --vis wandb \
---experiment-name ${SCENE_NAME} \
+--experiment-name (basename $data_path) \
 --steps-per-save 5000 \
 --max-num-iterations 30000 \
 --steps-per-eval-image 1000 \
@@ -26,5 +29,5 @@ ns-train depth-nerfacto-huge \
 --pipeline.model.predict-normals False \
 --pipeline.datamanager.camera-optimizer.mode off \
 nerfstudio-data \
---data /cluster/project/infk/courses/252-0579-00L/group26/sniall/kitti/datasets_cvpr/${SCENE_NAME} \
+--data $data_path \
 --depth-unit-scale-factor 0.00390625 \
