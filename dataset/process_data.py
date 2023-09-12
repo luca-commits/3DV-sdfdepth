d0e626834ecaf04b82b6813c2e5b44681dbc35a6
Niall Siegenheim <sniall@student.ethz.ch>
2023-09-12 12:26:19 +0200

    scripts for training

--- dataset/process_data.py
+++ dataset/process_data.py
@@ -1,20 +1,37 @@
 import os
 import math
 
-target_num_frames_per_clip = 100
+# with open('train_syncs.txt') as file:
+#     scenes = [line.rstrip() for line in file]
 
-with open('train_syncs.txt') as file:
-    scenes = [line.rstrip() for line in file]
+target_num_frames_per_clip = 100
     
-# scenes = ['2011_09_26_drive_0001_sync']
+scenes = ['2011_09_26_drive_0001_sync']
 
 for scene in scenes:
-    print(scene)
+    date = scene[:10]
+    drive = scene[-9:-5]
+    
+    print(date, drive)
 
-    os.makedirs(f"/cluster/project/infk/courses/252-0579-00L/group26/sniall/kitti/datasets_cvpr/{scene}", exist_ok=True)
 
-    os.system(f"python generate_nerfstudio_dataset.py \
-                --rgb-base-path /cluster/project/infk/courses/252-0579-00L/group26/kitti/rgb_images \
-                --depth-base-path /cluster/project/infk/courses/252-0579-00L/group26/kitti/depth/data_depth_annotated/train \
-                --scene {scene} \
-                --save-path /cluster/project/infk/courses/252-0579-00L/group26/sniall/kitti/datasets_cvpr/{scene}")
+    num_frames = len(os.listdir(f'/cluster/project/infk/courses/252-0579-00L/group26/sniall/kitti/images/{date}/{scene}/image_02/data/'))
+    
+    num_clips = math.ceil(num_frames / target_num_frames_per_clip)
+    num_frames_per_clip = num_frames // num_clips
+
+    for clip_idx in range(num_clips):
+        # Generate Nerfstudio dataset
+        start_frame = clip_idx * num_frames_per_clip
+        end_frame = (clip_idx + 1) * num_frames_per_clip
+        
+        print(num_frames, start_frame, end_frame)
+
+        os.makedirs(f"/cluster/project/infk/courses/252-0579-00L/group26/sniall/kitti/datasets_cvpr/{scene}_{clip_idx}", exist_ok=True)
+
+        os.system(f"python generate_nerfstudio_dataset.py \
+                    --rgb-base-path /cluster/project/infk/courses/252-0579-00L/group26/kitti/rgb_images \
+                    --depth-base-path /cluster/project/infk/courses/252-0579-00L/group26/kitti/depth/data_depth_annotated/train \
+                    --scene {scene} \
+                    --save-path /cluster/project/infk/courses/252-0579-00L/group26/sniall/kitti/datasets_cvpr/{scene}_{clip_idx} \
+                    --first_index {start_frame} --last_index {end_frame}")
--- training/nerfstudio/submit_training.sh
+++ training/nerfstudio/submit_training.sh
@@ -10,31 +10,21 @@
 #SBATCH --error=nerfstudio_training.err
 
 
-SCENE_NAME=2011_09_26_drive_0001_sync
+SCENE_NAME=2011_09_26_drive_0001_sync_0
 
 module purge
 module load gcc/8.2.0 cuda/11.8.0 python/3.9.9 eth_proxy
 source /cluster/project/infk/courses/252-0579-00L/group26/nerfstudio/venv/bin/activate
 
-hostname -i
 ns-train depth-nerfacto \
---vis viewer+wandb \
---output-dir ./output \
+--vis wandb \
+--experiment-name ${SCENE_NAME} \
 --steps-per-save 5000 \
---max-num-iterations 50000 \
+--max-num-iterations 30000 \
 --steps-per-eval-image 1000 \
---pipeline.model.depth-loss-mult 0.5 \
---pipeline.model.predict-normals True \
+--pipeline.model.depth-loss-mult 1.0 \
+--pipeline.model.predict-normals False \
 --pipeline.datamanager.camera-optimizer.mode off \
 nerfstudio-data \
 --data /cluster/project/infk/courses/252-0579-00L/group26/sniall/kitti/datasets_cvpr/${SCENE_NAME} \
---depth-unit-scale-factor 0.00390625
-
-# --data /cluster/project/infk/courses/252-0579-00L/group26/sniall/kitti/images/2011_09_26/2011_09_26_drive_0001_sync
-# sdfstudio-data \
-# --data /cluster/project/infk/courses/252-0579-00L/group26/sniall/kitti/datasets_cvpr/${SCENE_NAME} \
-# --include-mono-prior True \
-# --depth-unit-scale-factor 1. \
-
-
-
+--depth-unit-scale-factor 0.00390625 \
