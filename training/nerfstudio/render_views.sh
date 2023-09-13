292a44af667239339c7cabd1c0e58b80702e94ec
Niall Siegenheim <sniall@student.ethz.ch>
2023-09-13 16:09:36 +0200

    Updated rendering script

--- training/nerfstudio/render_views.sh
+++ training/nerfstudio/render_views.sh
@@ -1,25 +1,43 @@
 #!/bin/bash
 
 #SBATCH -c 8
-#SBATCH --time=8:00:00
+#SBATCH --time=1:00:00
 #SBATCH --mem-per-cpu=8G
 #SBATCH --gpus=rtx_3090:1
-#SBATCH -A s_stud_infk 
+#SBATCH -A s_stud_infk
 #SBATCH --job-name=novel_views
-#SBATCH --output=novel_views.out
-#SBATCH --error=novel_views.err
+#SBATCH --output=novel_views_%A_%a.out
+#SBATCH --array=0
 
-TIMESTAMP=2023-09-08_154019
-ANGLE=180
+angle=10
+
+datasets=(/cluster/project/infk/courses/252-0579-00L/group26/sniall/kitti/datasets_cvpr/*/)
+datasets=$(basename $(echo $datasets | xargs -n1 | sort | xargs))
+
+dataset=${datasets[$SLURM_ARRAY_TASK_ID]}
+
+models=(./outputs/$dataset/depth-nerfacto/*/)
+models=$(echo $models | xargs -n1 | sort -r | xargs)
+
+# Select most recent run
+config_path=${models[1]}config.yml
 
 module purge
 module load gcc/8.2.0 cuda/11.8.0 python/3.9.9 ffmpeg/5.0 eth_proxy
 source /cluster/project/infk/courses/252-0579-00L/group26/nerfstudio/venv/bin/activate
 
-ns-render interpolate \
---load-config output/unnamed/depth-nerfacto/${TIMESTAMP}/config.yml \
+ns-render angled \
+--load-config $config_path \
 --rendered-output-names rgb depth \
 --pose-source train \
---output-format video \
-# --angle 180
-# --colormap-options.colormap-max 0.2
+--output-format raw-separate \
+--output-path ./renders/$(basename ${models[1]})/pos_angle \
+--angle $angle
+
+ns-render angled \
+--load-config $config_path \
+--rendered-output-names rgb depth \
+--pose-source train \
+--output-format raw-separate \
+--output-path ./renders/$(basename ${models[1]})/neg_angle \
+--angle -$angle
