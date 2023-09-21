c41e6a39966892170f1e2af8834efee7fc5f2af2
LRabuzin <lovro.rabuzin@gmail.com>
2023-09-21 10:55:30 +0200

    change json params

--- dataset/crop_images.py
+++ dataset/crop_images.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import os
 import argparse
+import json
 
 def center_crop_image(image_path, crop_pixels):
     with Image.open(image_path) as img:
@@ -29,6 +30,13 @@ def center_crop_all_images(directory, crop_pixels):
             if file.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tif', '.tiff')):
                 image_path = os.path.join(root, file)
                 center_crop_image(image_path, crop_pixels)
+            if file.lower().endswith(('.json')):
+                params = json.load(open(os.path.join(root, file)))
+                params["cx"] -= crop_pixels
+                params["cy"] -= crop_pixels
+                params["w"] -= 2*crop_pixels
+                params["h"] -= 2*crop_pixels
+                json.dump(params, open(os.path.join(root, "cropped", file), "w"))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Crop given number of pixels on all sides of the image for all images in a folder')
