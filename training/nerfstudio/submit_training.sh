61dd4e8f60e11f21af3a21d3cb4b02494feb2456
Niall Siegenheim <sniall@student.ethz.ch>
2023-09-30 14:38:54 +0200

    Update training scrips for sweeping

--- training/nerfstudio/submit_training.sh
+++ training/nerfstudio/submit_training.sh
@@ -18,22 +18,4 @@ module purge
 module load gcc/8.2.0 cuda/11.8.0 python/3.9.9 eth_proxy
 source /cluster/project/infk/courses/252-0579-00L/group26/nerfstudio/venv/bin/activate
 
-python sweep.py --data $data_path
-
-# for depth_loss_mult in ${depth_loss_mults[@]}
-# do
-#     ns-train depth-nerfacto-huge \
-#     --vis wandb \
-#     --experiment-name $(basename $data_path)_$depth_loss_mult \
-#     --steps-per-save 5000 \
-#     --max-num-iterations 10000 \
-#     --steps-per-eval-image 1000 \
-#     --steps-per-eval-all-images 1000 \
-#     --pipeline.model.depth-loss-mult $depth_loss_mult \
-#     --pipeline.model.predict-normals False \
-#     --pipeline.datamanager.camera-optimizer.mode SO3xR3 \
-#     --pipeline.datamanager.images-on-gpu True \
-#     nerfstudio-data \
-#     --data $data_path \
-#     --depth-unit-scale-factor 0.00390625
-# done
+python train.py --data $data_path
