ccc8dc6132bc9f2a69f6e7ac2f5b9ed79b4dc1c1
Casimir-Feldmann <cfeldmann@student.ethz.ch>
2023-11-08 19:20:04 +0100

    updated to current rendering technique

--- dataset/prepare_data_MDE_toolbox.py
+++ dataset/prepare_data_MDE_toolbox.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 from tqdm import tqdm
 
-renders_dir = "/home/casimir/ETH/kitti/renders_50m_size"
+renders_dir = "/home/casimir/ETH/kitti/disturb_renders"
 rgb_dest = "/home/casimir/ETH/kitti/rgb_images"
 depth_dest = "/home/casimir/ETH/kitti/depth_mde"
 
@@ -19,40 +19,36 @@ with open(original_files_file_path) as original_files_file:
 files_file.write("\n")
 
 models = os.listdir(renders_dir)
-# models = models[:1]
-
-# breakpoint()
 
 for model in tqdm(models):
     model_path = f"{renders_dir}/{model}"
-    for angle in ["pos_angle", "neg_angle"]:
-        full_path = f"{model_path}/{angle}"
-        drive_split = model
-        drive = drive_split[:-2]
-        split = drive_split[-1]
-        date = drive[:10]
+    full_path = f"{model_path}"
+    drive_split = model
+    drive = drive_split[:-2]
+    split = drive_split[-1]
+    date = drive[:10]
 
-        # Copy RGBs
-        rgb_dest_full = f"{rgb_dest}/additional/{model}/{angle}/"
-        Path(rgb_dest_full).mkdir(parents=True, exist_ok=True)
-        os.popen(f'cp {full_path}/*_rgb.png {rgb_dest_full}')
+    # Copy RGBs
+    rgb_dest_full = f"{rgb_dest}/additional/{model}/"
+    Path(rgb_dest_full).mkdir(parents=True, exist_ok=True)
+    os.popen(f'cp {full_path}/*_rgb.png {rgb_dest_full}')
 
-        rgb_files = sorted(os.listdir(rgb_dest_full))
+    rgb_files = sorted(os.listdir(rgb_dest_full))
 
-        # print(rgb_files)
-        # breakpoint()
+    # print(rgb_files)
+    # breakpoint()
 
-        # Copy depths
-        depth_dest_full = f"{depth_dest}/additional/{model}/{angle}/"
-        Path(depth_dest_full).mkdir(parents=True, exist_ok=True)
-        os.popen(f'cp {full_path}/*_depth.png {depth_dest_full}')
+    # Copy depths
+    depth_dest_full = f"{depth_dest}/additional/{model}/"
+    Path(depth_dest_full).mkdir(parents=True, exist_ok=True)
+    os.popen(f'cp {full_path}/*_depth.png {depth_dest_full}')
 
-        depth_files = sorted(os.listdir(depth_dest_full))
+    depth_files = sorted(os.listdir(depth_dest_full))
 
-        print(len(depth_files), len(rgb_files))
+    print(len(depth_files), len(rgb_files))
 
-        # Write to file of rgbs and depth paths for training
-        for rgb_file, depth_file in zip(rgb_files, depth_files):
-            files_file.write(f"additional/{model}/{angle}/{rgb_file} additional/{model}/{angle}/{depth_file} 0.0\n")
+    # Write to file of rgbs and depth paths for training
+    for rgb_file, depth_file in zip(rgb_files, depth_files):
+        files_file.write(f"additional/{model}/{rgb_file} additional/{model}/{depth_file} 0.0\n")
 
 files_file.close()
